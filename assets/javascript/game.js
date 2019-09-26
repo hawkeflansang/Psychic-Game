b6aa7317258e9bd3aed380ac380ad9ca7703be1d
Hawke Lansang <hawkeflansang@gmail.com>
2019-09-26 10:06:57 -0400

    defined win conditions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
+var guessesMade = [];
 
 var winText = document.getElementById("win-text");
 var loseText = document.getElementById("lose-text");
@@ -12,11 +13,22 @@ var userGuesses = document.getElementById("user-guesses");
 winText.textContent = "Wins: " + wins;
 loseText.textContent = "Losses: " + losses;
 triesLeft.textContent = "Guesses Left: " + guessesLeft;
-userGuesses.textContent = "";
+userGuesses.textContent = guessesMade;
 
 document.onkeyup = function(event) {
 
     var userChoice = event.key;
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    
+    var validChoice = computerChoices.indexOf(userChoice);
+
+    if (validChoice !== -1) {
+
+        if (userChoice === computerGuess) {
+
+            wins++;
+        }
+
+    }
 
 }
