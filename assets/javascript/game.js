d1321fc47b9a9793bb647ab12852f7fff871136a
Hawke Lansang <hawkeflansang@gmail.com>
2019-09-25 22:14:39 -0400

    created game choice array and html id variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+
+var winText = document.getElementById("win-text");
+var loseText = document.getElementById("lose-text");
+var triesLeft = document.getElementById("tries-left");
